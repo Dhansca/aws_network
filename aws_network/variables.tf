6cca2855f6e42a19e83f957caf5eaa7efbedef3e
EC2 Default User <ec2-user@ip-172-31-40-119.ec2.internal>
2023-02-12 22:11:47 +0000

    variable edit

--- aws_network/variables.tf
+++ aws_network/variables.tf
@@ -1,20 +1,20 @@
 # Instance type
-variable "instance_type" {
-  default = {
-    "prod"    = "t2.micro"
-    "test"    = "t2.micro"
-    "staging" = "t2.micro"
-  }
-  description = "Type of the instance"
-  type        = map(string)
-}
+# variable "instance_type" {
+#   default = {
+#     "prod"    = "t2.micro"
+#     "test"    = "t2.micro"
+#     "staging" = "t2.micro"
+#   }
+#   description = "Type of the instance"
+#   type        = map(string)
+# }
 
 # Provision public subnets in custom VPC
-variable "public_subnet_cidrs" {
-  default     = ["10.10.0.0/24", "10.10.1.0/24"]
-  type        = list(string)
-  description = "Public Subnet CIDRs"
-}
+# variable "public_subnet_cidrs" {
+#   default     = ["10.10.0.0/24", "10.10.1.0/24"]
+#   type        = list(string)
+#   description = "Public Subnet CIDRs"
+# }
 
 # Provision public subnets in custom VPC
 variable "public_cidr_blocks" {
@@ -32,17 +32,14 @@ variable "vpc_cidr" {
 
 # Default tags
 variable "default_tags" {
-  default = {
-    "Owner" = "CAAacs730"
-    "App"   = "Web"
-  }
+  default = {}
   type        = map(any)
   description = "Default tags to be appliad to all AWS resources"
 }
 
 # Prefix to identify resources
 variable "prefix" {
-  default     = "week5"
+  #default     = "week6"
   type        = string
   description = "Name prefix"
 }
@@ -50,7 +47,7 @@ variable "prefix" {
 
 # Variable to signal the current environment 
 variable "env" {
-  default     = "test"
+  default     = "dev"
   type        = string
   description = "Deployment Environment"
 }
